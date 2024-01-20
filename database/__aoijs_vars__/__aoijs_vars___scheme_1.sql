1848e8f9095189785c504f86d81757a98d6742fb
Bird Tool <93103818+MrExQuiloBr@users.noreply.github.com>
2024-01-20 10:24:14 -0300

    Add files via upload

---
+++ database/__aoijs_vars__/__aoijs_vars___scheme_1.sql
@@ -0,0 +1,1 @@
+{"cooldown_trabalhar_1171963692984844401":{"value":"1705683736471","type":"number","key":"cooldown_trabalhar_1171963692984844401"},"cooldown_trabalhar_1021204685782257775":{"value":"1705587225545","type":"number","key":"cooldown_trabalhar_1021204685782257775"}}
